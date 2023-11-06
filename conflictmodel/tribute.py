34821d0385d7aded2a2c30507fc702b0d179a165
brandonm <brandon.minta@yachaytech.edu.ec>
2023-11-05 20:36:43 -0500

    Final Version, included path legth and changes in the loyalty matrix

--- conflictmodel/tribute.py
+++ conflictmodel/tribute.py
@@ -24,10 +24,8 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Run the simulation script.')
     parser.add_argument('-N', type=int, default=100, help='Number of actors')
     parser.add_argument('--years', type=int, default=1000, help='Number of iterations')
-    parser.add_argument("--r", type=int, default=20, help="Value of productivity/income")
-    parser.add_argument("--q", type=int, default=250, help="Value of tribute")
-    parser.add_argument("--network_type", choices=["watts_strogatz", "grid_2d"], default="grid_2d", help="Type of network")
-    parser.add_argument('--ncpu', type=int, default=4, help='Number of processes')
+    parser.add_argument("--network_type", choices=["watts_strogatz", "grid_2d", "complete", "wheel"], default="grid_2d", help="Type of network")
+    parser.add_argument('--ncpu', type=int, default=1, help='Number of processes')
     parser.add_argument("--output_dir", required=True, help="Output directory")
     return parser.parse_args()
 #Constants     
@@ -35,9 +33,9 @@ args =  parse_arguments()
 c = 1                           # commitment fluctuation 
 k = 0.25                        # Destructiveness 
 demands = 3                     # Demands per Year Cycle (1/3 of N)
-period_steps = 10               # Period for data collection
-r = args.r                      # harvest
-q = args.q                      # Tribute
+period_steps = 1               # Period for data collection
+r = 20                          # harvest
+q = 250                         # Tribute
 
 # Functions --------------------------------------------------------------------------
 def saving_data(rank, output_dir, loyalty_list, tribute_list, data_matrix):
@@ -82,6 +80,10 @@ def create_graph(num_nodes, graph_type='grid', **kwargs):
         k = kwargs.get('k', 4)  # Default value for k is set to 4
         p = kwargs.get('p', 0.1)  # Default value for p is set to 0.1
         G = nx.watts_strogatz_graph(num_nodes, k, p)
+    elif graph_type == 'complete':
+        G = nx.complete_graph(num_nodes)
+    elif graph_type == 'wheel':
+        G = nx.wheel_graph(num_nodes)
     return G
      
 def loss(coAWealth, coBWealth, contribution):         
@@ -123,69 +125,106 @@ def group_resources(agent, coalition_arr, capital_arr, loyalty_mtx):
     return money  
 
 def candidate_connection(G,loyalty_mtx, attacker, target):
-    # Create a subgraph of to remove nodes
+    """
+    This function checks is there is a path between Attacker and target
+    """
+    # Create a subgraph to remove nodes
     H = G.copy()
     nodes_to_isolate = []
     for node in range(len(loyalty_mtx)):
         conditionA = loyalty_mtx[node, attacker]
         conditionB = loyalty_mtx[node, target] 
         if (conditionA <= conditionB ) and node not in [attacker, target]:
+        #if (conditionA <= conditionB or  conditionA < 5) and node not in [attacker, target]:  #
             nodes_to_isolate.append(node)
     H.remove_nodes_from(nodes_to_isolate)
     #Check if there is a path between nodes
-    return nx.has_path(H, attacker, target)
+    if nx.has_path(H, attacker, target):
+        path_length = nx.shortest_path_length(H, source=attacker, target=target)
+        return path_length
+    else: 
+        return None
 
 def group(G, a, b, loyalty_mtx):
+    """
+    This function find the spatial connected coalition of 'a'
+    """
     # Create an initial grouping where each node is its own group
     group_a = {node for node in G.nodes() if (loyalty_mtx[node, a] > loyalty_mtx[node, b] and node != b) or (node == a)}
+    #group_a = {node for node in G.nodes() if (loyalty_mtx[node, a] > loyalty_mtx[node, b] and node != b and loyalty_mtx[node, a] >=0.5) or (node == a)}
     subgraph = G.subgraph(group_a) 
     coalition = list(nx.descendants(subgraph, a))+ [a]
     return np.array(coalition, dtype=int)  
 
-def candidate_exposure(G, attacker, target, capital, loyalty_mtx):
+def candidate_exposure(G, attacker, target, capital, loyalty_mtx, path_length):
+    """
+    This function calculates the susceptibility 
+    """
     attacker_alley = group(G, attacker, target , loyalty_mtx)
     target_alley = group(G, target, attacker, loyalty_mtx)
     w_att = group_resources(attacker, attacker_alley, capital, loyalty_mtx)
     w_def = group_resources(target, target_alley, capital, loyalty_mtx)
+    #susceptibility = vulnerability(w_att, w_def) * min(q,capital[target])/(path_length**(1/4))      #Variation Path length penalization
     susceptibility = vulnerability(w_att, w_def) * min(q,capital[target])
     return susceptibility, attacker_alley, target_alley, w_att, w_def
 
-def candidate_selection(G,loyalty_mtx, attacker, capital):
-    neighbors_attacker = list(G.neighbors(attacker))  
-    susceptibility, attacker_alley, target_alley, w_att, w_def = candidate_exposure(G, attacker, neighbors_attacker[0], capital, loyalty_mtx)
-    optimal = {
-    "susceptibility": susceptibility,
-    "attacker_alley": attacker_alley,
-    "target_alley": target_alley,
-    "w_att": w_att,
-    "w_def": w_def,
-    "attacker": attacker,
-    "target": neighbors_attacker[0]}
+def candidate_selection(G, loyalty_mtx, attacker, capital):
+    """
+    This function selects the best target possible if any
+    """
+    neighbors_attacker = list(G.neighbors(attacker))
+    
+    def get_optimal_params(target):
+        """
+        Helper function to calculate susceptibility and other parameters for a target node.
+        """
+        path_length = candidate_connection(G, loyalty_mtx, attacker, target)
+        # Exclude not profitable targets and not spatially connected. 
+        if capital[target] < 0.1 or path_length is None: 
+        #if capital[target] < 0.1: 
+            return {
+            "susceptibility": 0,
+            "attacker_alley": None,
+            "target_alley": None,
+            "w_att": None,
+            "w_def": None,
+            "target": None,
+            "attacker":None, 
+            "path_len":None
+        }
+        # Calculate parameters using candidate_exposure function
+        susceptibility, attacker_alley, target_alley, w_att, w_def = candidate_exposure(G, attacker, target, capital, loyalty_mtx, path_length)
+        return {
+            "susceptibility": susceptibility,
+            "attacker_alley": attacker_alley,
+            "target_alley": target_alley,
+            "w_att": w_att,
+            "w_def": w_def,
+            "target": target,
+            "attacker":attacker,
+            "path_len":path_length
+        }
+    
+    # Initialize optimal parameters with the first neighbor of the attacker
+    optimal = get_optimal_params(neighbors_attacker[0])   
+    # Iterate through other neighbors of the attacker and update optimal parameters if a better target is found
     for target in neighbors_attacker[1:]:
-        if capital[target] < 0.1:
-            continue
-        susceptibility, attacker_alley, target_alley, w_att, w_def = candidate_exposure(G, attacker, target, capital, loyalty_mtx)
-        if susceptibility > optimal["susceptibility"]:
-            optimal["susceptibility"] = susceptibility
-            optimal["attacker_alley"] = attacker_alley
-            optimal["target_alley"] = target_alley
-            optimal["w_att"] = w_att
-            optimal["w_def"] = w_def 
-            optimal["target"] = target          
+        params = get_optimal_params(target)
+        # Check if the target is valid and has higher susceptibility
+        if params and params["susceptibility"] > optimal["susceptibility"]:
+            optimal.update(params)  
+            
+    # Iterate through all nodes that are not neighbors of the attacker and update optimal parameters if a better target is found
     for target in [node for node in range(len(loyalty_mtx)) if node not in neighbors_attacker and node != attacker]:
-        if capital[target] < 0.1:
+        # Check if target's neighbors are not commited with attacker (no possible path)
+        if all(loyalty_mtx[node][attacker] <= loyalty_mtx[node][target] for node in list(G.neighbors(target))):
             continue
-        neighbors_target = list(G.neighbors(target))        
-        if all(loyalty_mtx[node][attacker] <= loyalty_mtx[node][target] for node in neighbors_target):
-            continue
-        susceptibility, attacker_alley, target_alley, w_att, w_def = candidate_exposure(G, attacker, target, capital, loyalty_mtx)
-        if susceptibility > optimal["susceptibility"]:
-            optimal["susceptibility"] = susceptibility
-            optimal["attacker_alley"] = attacker_alley
-            optimal["target_alley"] = target_alley
-            optimal["w_att"] = w_att
-            optimal["w_def"] = w_def 
-            optimal["target"] = target  
+        params = get_optimal_params(target)
+        # Update optimal parameters if a better target is found
+        if params and params["susceptibility"] > optimal["susceptibility"]:
+            optimal.update(params)
+    
+    # Return the optimal parameters
     return optimal
 
 def response(optimal, capital, loyalty_mtx, tribute_mtx):
@@ -202,7 +241,7 @@ def response(optimal, capital, loyalty_mtx, tribute_mtx):
         for i in target_alley:
             offering = 0.1*loyalty_mtx[i][target] * capital[i]
             contribution_loss = damage_by_attacker*offering/w_def
-            capital[i] -= round(contribution_loss,1)
+            capital[i] -= contribution_loss
             for m in target_alley:
                 if 10 - c >= loyalty_mtx[i][m] >= 0:
                     loyalty_mtx[i][m] = loyalty_mtx[i][m] + c
@@ -214,13 +253,13 @@ def response(optimal, capital, loyalty_mtx, tribute_mtx):
         for j in attacker_alley:
             offering = 0.1*loyalty_mtx[j][attacker] * capital[j]
             contribution_loss = damage_by_defender*offering/w_att
-            capital[j] -= round(contribution_loss,1) 
+            capital[j] -= contribution_loss
             for l in attacker_alley:
                 if 10 - c >= loyalty_mtx[j][l] >= 0:
                     loyalty_mtx[j][l] = loyalty_mtx[j][l] + c
         return 1, loyalty  # Conflict: True ; Loyalty between attacker and target
     else:
-        money = round(min(q,capital[target]),1)
+        money = min(q,capital[target])
         capital[target], capital[attacker] = capital[target] - money, capital[attacker] + money
         tribute_mtx[target][attacker] += 1
         if 10 - c >= loyalty_mtx[target][attacker] >= 0 :
@@ -253,36 +292,41 @@ class Simulation:
         total_iterations = self.years * (self.N // demands)
         #Initialize the resources for each actor
         for i in range(self.N):
-            self.capital[i] = round(random.randrange(300,500,1), 1)
+            self.capital[i] = random.randrange(300,500,1)
         loyalty_list, tribute_list = [], []  # Lists for periodic loyalty, tribute matrices
-        data_matrix = np.zeros((total_iterations+1, self.N + 8), dtype=np.float32)  
-        data_matrix[0,8:] = self.capital
+        data_matrix = np.zeros((total_iterations+1, self.N + 10), dtype=np.float32)  
+        data_matrix[0,10:] = self.capital
         #Create the charging bar if pbar is provided
         if pbar:
             pbar.reset(total=total_iterations)
         # Loop for each simulation run 
-        iterator, period = 1, period_steps -1
+        iterator, period = 1, period_steps
         for year in range(self.years):
             if self.rank == 1:
-                if year == 0:
-                    loyalty_list.append(np.copy(self.loyalty_mtx))
-                    tribute_list.append(np.copy(self.tribute_mtx))
-                elif year == period:
-                    loyalty_list.append(np.copy(self.loyalty_mtx))
-                    tribute_list.append(np.copy(self.tribute_mtx))
-                    period += period_steps               
+                loyalty_list.append(np.copy(self.loyalty_mtx))
+                tribute_list.append(np.copy(self.tribute_mtx))
+            loyalty_prev = np.copy(self.loyalty_mtx)
             for k in range( self.N // demands):
                 decision, loyalty, optimal = self.simulate_activation()
                 if decision is not None:
+                    # Perform element-wise comparison and count the number of differing elements
+                    differences = loyalty_prev != self.loyalty_mtx
+                    num_differences = np.sum(differences)
                     info = np.array([decision, optimal["attacker"], optimal["target"], loyalty, 
-                            len(optimal["target_alley"] ), len(optimal["attacker_alley"]), optimal["w_def"], optimal["w_att"] ])
+                            len(optimal["target_alley"] ), len(optimal["attacker_alley"]), optimal["w_def"], 
+                            optimal["w_att"], optimal["path_len"],num_differences ])
                 else:
-                    info = np.full(8, None)
-                data_matrix[iterator,:8], data_matrix[iterator,8:] = info, self.capital
+                    info = np.full(10, None)
+                data_matrix[iterator,:10], data_matrix[iterator,10:] = info, self.capital
                 iterator += 1
                 if pbar:
                     pbar.update(1)
             self.capital += r
+           
+            # Calculate the increment amount (5% of each element's value)    #Proportional to what they have
+            #increment = 0.05 * self.capital
+            #self.capital += increment
+            
         loyalty_list.append(np.copy(self.loyalty_mtx))
         tribute_list.append(np.copy(self.tribute_mtx))
         return loyalty_list, tribute_list, data_matrix
@@ -316,6 +360,3 @@ if __name__ == "__main__":
     pool.starmap(run, parameters)
     # Close the pool
     pool.close()
-    
-    
-    
